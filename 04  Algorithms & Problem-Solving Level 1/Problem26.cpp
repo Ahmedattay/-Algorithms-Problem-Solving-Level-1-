9ada1b24cd413676836909a10741c0cb72c8fcdf
Ahmed Attay <ahmedattay8@gmail.com>
2025-06-08 18:37:27 +0300

    Add implementation of Problem26 to print numbers from 1 to user-defined maximum

---
+++ 04  Algorithms & Problem-Solving Level 1/Problem26.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int MAxnum;
+    cout << "Enter the maximum number: ";
+    cin >> MAxnum;
+    for (int i = 1; i <= MAxnum; i++)
+    {
+        cout << i << " ";
+    }
+    return 0;
+}
