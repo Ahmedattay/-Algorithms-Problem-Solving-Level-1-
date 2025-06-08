19d59daf0d00b2eb68f1f3c1f5e431bc809c382b
Ahmed Attay <ahmedattay8@gmail.com>
2025-06-08 16:56:40 +0300

    Add initial implementation of Problem1 with user input functionality

---
+++ 04  Algorithms & Problem-Solving Level 1/Problem1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string name;
+    cout << "Enter your name: ";
+    getline(cin, name);
+    cout << "your name is: " << name << endl;
+    return 0;
+}
