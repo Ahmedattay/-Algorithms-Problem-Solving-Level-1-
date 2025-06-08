d630f185342a6e9dc54361bfe65d44bc99332133
Ahmed Attay <ahmedattay8@gmail.com>
2025-06-08 18:28:17 +0300

    Add implementation of Problem22 to calculate and display the area of a circle inscribed in an isosceles triangle based on user input

---
+++ 04  Algorithms & Problem-Solving Level 1/Problem22.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// Function to calculate area of circle inscribed in an isosceles triangle
+// Given base (b) and equal side (a)
+double inscribedCircleArea(double a, double b) {
+    // Height from base to apex
+    double h = sqrt(a * a - (b * b) / 4.0);
+    // Area of triangle
+    double area = (b * h) / 2.0;
+    // Semi-perimeter
+    double s = (a + a + b) / 2.0;
+    // Radius of inscribed circle
+    double r = area / s;
+    // Area of circle
+    return M_PI * r * r;
+}
+
+int main() {
+    double a, b;
+    cout << "Enter the length of the equal sides (a): ";
+    cin >> a;
+    cout << "Enter the length of the base (b): ";
+    cin >> b;
+
+    if (a <= 0 || b <= 0 || b >= 2 * a) {
+        cout << "Invalid triangle dimensions." << endl;
+        return 1;
+    }
+
+    double circleArea = inscribedCircleArea(a, b);
+    cout << "Area of the inscribed circle: " << circleArea << endl;
+    return 0;
+}
