4290ac0d2f52e218eff637b154b863336c2ff739
Ahmed Attay <ahmedattay8@gmail.com>
2025-06-08 17:20:51 +0300

    Add implementation of Problem7 to calculate and display half of a user-input number

---
+++ 04  Algorithms & Problem-Solving Level 1/Problem7.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int Firstnumber;
+    cout << "Enter first number: ";
+    cin >> Firstnumber;
+    int Halfnumber = Firstnumber / 2;
+    cout << "Half of " << Firstnumber << " is: " << Halfnumber << endl;
+    return 0;
+}
